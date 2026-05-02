a536e507bcbd184b069000f269804f08189789d9
milan-1936 <khpo_bca2077@lict.edu.np>
2026-05-02 09:23:14 +0545

    first commit

---
+++ src/test/java/com/milan/springsecdemo/SpringSecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milan.springsecdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
